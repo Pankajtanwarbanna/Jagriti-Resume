f24e4fefd9ece545eec22132cb65552d475f3c25
Pankaj Tanwar Banna <2016ucp1381@mnit.ac.in>
2019-07-07 09:51:22 +0530

    Name added

---
+++ index.js
@@ -0,0 +1,15 @@
+const chalk = require('chalk');
+const figlet = require('figlet');
+const clear = require('clear');
+
+function main() {
+    clear();
+    console.log(
+        chalk.yellow(
+            figlet.textSync('Jagriti Aggarwal', { horizontalLayout: 'full' })
+        )
+    );  //resumeHandler();
+}
+
+main();
+
